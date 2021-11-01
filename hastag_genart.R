f9e76f3f9e34c27679a75a8c3a9f7b7dcc116e3b
Arthur Thina <arthur.thina@gmail.com>
2021-11-01 00:53:36 -0300

    add viridis

--- hastag_genart.R
+++ hastag_genart.R
@@ -1,13 +1,10 @@
-# Hello twitter
-
 library(rtweet)
 library(tidyverse)
 library(hrbrthemes)
 library(tidytext)
-library(RColorBrewer)
-library(scales)
+library(viridis)
 
-gen_twts <- search_tweets("#generative", n = 18000, include_rts = FALSE)
+gen_twts <- search_tweets("#generativeart", n = 18000, include_rts = FALSE)
 
 hash <- gen_twts %>% 
   unnest_tokens(hashtag, text, "tweets", to_lower = TRUE) %>%
@@ -15,19 +12,14 @@ hash <- gen_twts %>%
   count(hashtag, sort = TRUE) %>%
   top_n(15)
 
-colourCount = length(unique(hash$hashtag))
-getPalette = colorRampPalette(brewer.pal(9, "Set2"))
 
-p <- ggplot(hash, aes(x = reorder(hashtag, n), y = n, fill = hashtag)) +
+ggplot(hash, aes(x = reorder(hashtag, n), y = n, fill = hashtag)) +
   geom_col() +
-  scale_fill_manual(values = getPalette(colourCount)) +
-  scale_y_continuous(breaks = seq(from = 0, to = 1500, by = 250)) +
+  scale_y_continuous(breaks = seq(from = 0, to = 8000, by = 1000)) +
+  scale_fill_viridis(discrete = TRUE, option = "D") +
   labs(y = "Tweets",
        x = "Hashtag",
-       title = "Top 15 hashtags related to #Generative on Twitter",
-       subtitle = "(2020/09/07 - 2020/09/16)",
        caption = "@arthurstats_") +
   coord_flip() +
-  theme_ft_rc() 
-
-p + theme(legend.position = "none")
+  theme_ft_rc() +
+  theme(legend.position = "none")
